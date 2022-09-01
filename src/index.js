a3fbb49085ee0da7689722000ed2372d124204f8
Victor Mayi <victormay.i.797@gmail.com>
2022-09-01 13:18:47 +0300

    Results column population

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ findUser.addEventListener('submit', (e)=>{
 	.then(data => {
         console.log(data)
         let playlists = data.playlists.items
-        playlists.forEach(user => console.log(user.data.owner.name))
+        // playlists.forEach(user => console.log(user.data.owner.name))
         let goal = playlists.find(user => user.data.owner.name == e.target.user.value || user.data.owner.name.split(' ')[0, 2] == e.target.user.value)
         let username = goal.data.owner.username
         fetch(`https://spotify81.p.rapidapi.com/user_profile?id=${username}&playlistLimit=50&artistLimit=10`, options)
@@ -36,6 +36,7 @@ findUser.addEventListener('submit', (e)=>{
             let playlists = document.querySelector('#playlists')
             let lastId = parseInt(playlists.lastElementChild.id)
             info.public_playlists.forEach((pList) => {
+                // console.log(pList.name)
                 document.querySelector('#playlists').parentNode.parentNode.className = "flex-container black"
                 let div = document.createElement('div')
                 let img = document.createElement('img')
@@ -93,22 +94,68 @@ findUser.addEventListener('submit', (e)=>{
 	.catch(err => console.error(err));
 })
 
+const results = document.querySelector('#results')
 const playlists = document.querySelector('#playlists')
 playlists.addEventListener('click', (e)=>{
-    console.log(e.target.parentNode)
-    console.log(e.target.parentNode.lastElementChild.id)
-    fetch(`https://spotify81.p.rapidapi.com/playlist_tracks?id=${e.target.parentNode.lastElementChild.id}&offset=0&limit=100`, options)
+    // console.log(e.target.parentNode.parentNode.lastElementChild.lastElementChild.id)
+    fetch(`https://spotify81.p.rapidapi.com/playlist_tracks?id=${e.target.parentNode.parentNode.lastElementChild.lastElementChild.id}&offset=0&limit=100`, options)
 	.then(res => res.json())
-	.then(data => console.log(data))
+	.then(data => {
+        console.log(data)
+        let lastId = parseInt(results.lastElementChild.id)
+        results.innerHTML = '<div class="result disabled" id="0"></div>'
+        data.items.forEach(song => {
+            let div = document.createElement('div')
+            let img = document.createElement('img')
+            let p = document.createElement('p')
+            let div1 = document.createElement('div')
+            let div2 = document.createElement('div')
+            img.src = `${song.track.album.images[2].url}`
+            img.class = 'result-image'
+            p.innerHTML = `${song.track.name}`
+            p.id = song.track.id
+            div1.appendChild(img)
+            div1.className = 'flex-child'
+            div2.appendChild(p)
+            div2.className = 'flex-child'
+            div.appendChild(div1)
+            div.appendChild(div2)
+            div.id = lastId +=1
+            div.className = 'playlist flex-container'
+            results.appendChild(div)
+        })
+    })
 	.catch(err => console.error(err));
 })
 
 const recommended = document.querySelector('#recommended')
 recommended.addEventListener('click', (e)=>{
-    console.log(e.target.parentNode)
-    console.log(e.target.parentNode.lastElementChild.id)
-    fetch(`https://spotify81.p.rapidapi.com/playlist_tracks?id=${e.target.parentNode.lastElementChild.id}&offset=0&limit=100`, options)
+    fetch(`https://spotify81.p.rapidapi.com/playlist_tracks?id=${e.target.parentNode.parentNode.lastElementChild.lastElementChild.id}&offset=0&limit=100`, options)
 	.then(res => res.json())
-	.then(data => console.log(data))
+	.then(data => {
+        console.log(data)
+        let lastId = parseInt(results.lastElementChild.id)
+        results.innerHTML = '<div class="result disabled" id="0"></div>'
+        data.items.forEach(song => {
+            let div = document.createElement('div')
+            let img = document.createElement('img')
+            let p = document.createElement('p')
+            let div1 = document.createElement('div')
+            let div2 = document.createElement('div')
+            img.src = `${song.track.album.images[2].url}`
+            img.class = 'result-image'
+            p.innerHTML = `${song.track.name}`
+            p.id = song.track.id
+            div1.appendChild(img)
+            div1.className = 'flex-child'
+            div2.appendChild(p)
+            div2.className = 'flex-child'
+            div.appendChild(div1)
+            div.appendChild(div2)
+            div.id = lastId +=1
+            div.className = 'playlist flex-container'
+            results.appendChild(div) 
+        })
+    })
 	.catch(err => console.error(err));
 })
