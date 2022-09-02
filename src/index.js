2a5cb840c3031826c999e1ddc45d8f5b842c0908
Victor Mayi <victormay.i.797@gmail.com>
2022-09-02 15:56:08 +0300

    Eliminated possibility of empty submits

--- src/index.js
+++ src/index.js
@@ -11,88 +11,98 @@ findUser.addEventListener('submit', (e)=>{
     e.preventDefault()
     // console.log(e.target.user.value)
     // console.log(e.target.playlist.value)
-    fetch(`https://spotify81.p.rapidapi.com/search?q=${e.target.playlist.value}&type=playlists&offset=0&limit=100&numberOfTopResults=25`, options)
-	.then(response => response.json())
-	.then(data => {
-        console.log(data)
-        let playlists = data.playlists.items
-        // playlists.forEach(user => console.log(user.data.owner.name))
-        let goal = playlists.find(user => user.data.owner.name.toString().toLowerCase() == e.target.user.value.toString().toLowerCase() || user.data.owner.name.split(' ')[0, 2] == e.target.user.value)
-        let username = goal.data.owner.username
-        fetch(`https://spotify81.p.rapidapi.com/user_profile?id=${username}&playlistLimit=50&artistLimit=10`, options)
-        .then(res => res.json())
-        .then(info => {
-            console.log(info)
-            if(info.name != false){
-                document.querySelector("#username").innerHTML = `${info.name}`
-            }
-            document.querySelector("#username").parentNode.className = "flex-child"
+    if(e.target.playlist.value.length > 0 && e.target.playlist.value.trim().length > 0){
+        fetch(`https://spotify81.p.rapidapi.com/search?q=${e.target.playlist.value}&type=playlists&offset=0&limit=100&numberOfTopResults=25`, options)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            let playlists = data.playlists.items
+            // playlists.forEach(user => console.log(user.data.owner.name))
+            if(e.target.user.value.length > 0 && e.target.user.value.trim().length > 0){
+                let goal = playlists.find(user => user.data.owner.name.toString().toLowerCase() == e.target.user.value.toString().toLowerCase() || user.data.owner.name.split(' ')[0, 2] == e.target.user.value)
+                let username = goal.data.owner.username
+                fetch(`https://spotify81.p.rapidapi.com/user_profile?id=${username}&playlistLimit=50&artistLimit=10`, options)
+                .then(res => res.json())
+                .then(info => {
+                    console.log(info)
+                    if(info.name != false){
+                        document.querySelector("#username").innerHTML = `${info.name}`
+                    }
+                    document.querySelector("#username").parentNode.className = "flex-child"
 
-            if(info.image_url != false){
-                document.querySelector("#user-image").src = info.image_url
+                    if(info.image_url != false){
+                        document.querySelector("#user-image").src = info.image_url
+                    }
+                    document.querySelector("#user-image").parentNode.className = "flex-child"
+                    document.querySelector("#find-user").parentNode.className = "flex-child disabled"
+                    let playlists = document.querySelector('#playlists')
+                    let lastId = parseInt(playlists.lastElementChild.id)
+                    info.public_playlists.forEach((pList) => {
+                        // console.log(pList.name)
+                        document.querySelector('#playlists').parentNode.parentNode.className = "flex-container black"
+                        let div = document.createElement('div')
+                        let img = document.createElement('img')
+                        let p = document.createElement('p')
+                        let div1 = document.createElement('div')
+                        let div2 = document.createElement('div')
+                        img.src = `https://i.scdn.co/image/${pList.image_url.split(':')[2]}`
+                        img.class = 'playlist-image'
+                        p.innerHTML = `${pList.name}`
+                        p.id = pList.uri.split(':')[2]
+                        div1.appendChild(img)
+                        div1.className = 'flex-child image'
+                        div2.appendChild(p)
+                        div2.className = 'flex-child title'
+                        div.appendChild(div1)
+                        div.appendChild(div2)
+                        div.id = lastId +=1
+                        div.className = 'playlist flex-container'
+                        playlists.appendChild(div)
+                    })
+                    e.target.reset()
+                })
+                .catch(err => console.error(err));
+
+                fetch('https://spotify81.p.rapidapi.com/user_profile?id=spotify&playlistLimit=44&artistLimit=10', options)
+                .then(res => res.json())
+                .then(info => {
+                    console.log(info)
+                    let playlists = document.querySelector('#recommended')
+                    let lastId = parseInt(playlists.lastElementChild.id)
+                    info.public_playlists.forEach((pList) => {
+                        document.querySelector('#playlists').parentNode.parentNode.className = "flex-container black"
+                        let div = document.createElement('div')
+                        let img = document.createElement('img')
+                        let p = document.createElement('p')
+                        let div1 = document.createElement('div')
+                        let div2 = document.createElement('div')
+                        img.src = `https://i.scdn.co/image/${pList.image_url.split('/')[4]}`
+                        img.class = 'playlist-image'
+                        p.innerHTML = `${pList.name}`
+                        p.id = pList.uri.split(':')[2]
+                        div1.appendChild(img)
+                        div1.className = 'flex-child image'
+                        div2.appendChild(p)
+                        div2.className = 'flex-child title'
+                        div.appendChild(div1)
+                        div.appendChild(div2)
+                        div.id = lastId +=1
+                        div.className = 'playlist flex-container'
+                        playlists.appendChild(div)
+                        document.querySelector('#user-container').className = 'flex-container user'
+                    })
+                })
+                .catch(err => console.error(err));
             }
-            document.querySelector("#user-image").parentNode.className = "flex-child"
-            document.querySelector("#find-user").parentNode.className = "flex-child disabled"
-            let playlists = document.querySelector('#playlists')
-            let lastId = parseInt(playlists.lastElementChild.id)
-            info.public_playlists.forEach((pList) => {
-                // console.log(pList.name)
-                document.querySelector('#playlists').parentNode.parentNode.className = "flex-container black"
-                let div = document.createElement('div')
-                let img = document.createElement('img')
-                let p = document.createElement('p')
-                let div1 = document.createElement('div')
-                let div2 = document.createElement('div')
-                img.src = `https://i.scdn.co/image/${pList.image_url.split(':')[2]}`
-                img.class = 'playlist-image'
-                p.innerHTML = `${pList.name}`
-                p.id = pList.uri.split(':')[2]
-                div1.appendChild(img)
-                div1.className = 'flex-child image'
-                div2.appendChild(p)
-                div2.className = 'flex-child title'
-                div.appendChild(div1)
-                div.appendChild(div2)
-                div.id = lastId +=1
-                div.className = 'playlist flex-container'
-                playlists.appendChild(div)
-            })
-            e.target.reset()
+            else{
+                window.alert('Name isn\'t valid')
+            }
         })
         .catch(err => console.error(err));
-
-        fetch('https://spotify81.p.rapidapi.com/user_profile?id=spotify&playlistLimit=44&artistLimit=10', options)
-        .then(res => res.json())
-        .then(info => {
-            console.log(info)
-            let playlists = document.querySelector('#recommended')
-            let lastId = parseInt(playlists.lastElementChild.id)
-            info.public_playlists.forEach((pList) => {
-                document.querySelector('#playlists').parentNode.parentNode.className = "flex-container black"
-                let div = document.createElement('div')
-                let img = document.createElement('img')
-                let p = document.createElement('p')
-                let div1 = document.createElement('div')
-                let div2 = document.createElement('div')
-                img.src = `https://i.scdn.co/image/${pList.image_url.split('/')[4]}`
-                img.class = 'playlist-image'
-                p.innerHTML = `${pList.name}`
-                p.id = pList.uri.split(':')[2]
-                div1.appendChild(img)
-                div1.className = 'flex-child image'
-                div2.appendChild(p)
-                div2.className = 'flex-child title'
-                div.appendChild(div1)
-                div.appendChild(div2)
-                div.id = lastId +=1
-                div.className = 'playlist flex-container'
-                playlists.appendChild(div)
-                document.querySelector('#user-container').className = 'flex-container user'
-            })
-        })
-        .catch(err => console.error(err));
-    })
-	.catch(err => console.error(err));
+    }
+    else{
+        window.alert('Search wasn\'t executed')
+    }
 })
 
 const results = document.querySelector('#results')
